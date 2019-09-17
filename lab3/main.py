2951a630c4f47c7a1a2f7deabe9a8ab079c5f208
Isabelle Ferreira <isabelle.ferreira3000@gmail.com>
2019-09-16 21:53:13 -0300

    ordenou a lista de generos por userID

--- lab3/main.py
+++ lab3/main.py
@@ -3,6 +3,7 @@ import numpy as np
 import ast
 
 # READING MOVIES, RATINGS AND USERS DATA
+print("READING MOVIES, RATINGS AND USERS DATA")
 movies_data = pd.read_csv('ml-1m/movies.dat', delimiter="::", engine='python')
 movies_data = movies_data.dropna(axis=0, how='any')
 
@@ -15,18 +16,22 @@ users_data = users_data.dropna(axis=0, how='any')
 users_data = users_data.drop(columns=['Zip-code'])  # removing Zip-code column
 
 # MERGING MOVIES, RATINGS AND USERS DATA
+print("MERGING MOVIES, RATINGS AND USERS DATA")
 merged_data = pd.merge(users_data, ratings_data, how='inner', on='UserID')
 merged_data = pd.merge(merged_data, movies_data, how='inner', on='MovieID')
 
 # GETTING 5 OR 4 STARS MOVIES FROM MERGED_DATA
+print("GETTING 5 OR 4 STARS MOVIES FROM MERGED_DATA")
 data_with_five_stars = merged_data.loc[merged_data['Rating'] == 5]
 data_with_four_stars = merged_data.loc[merged_data['Rating'] == 4]
 merged_data_five_or_four_stars = pd.concat([data_with_five_stars, data_with_four_stars])
 
 # CREATING GENRES COLUMN ON USERS DATA
+print("CREATING GENRES COLUMN ON USERS DATA")
 users_data['Genres'] = np.nan
 
 # FILLING GENRES COLUMN ON USERS DATA
+print("FILLING GENRES COLUMN ON USERS DATA")
 for userID in merged_data_five_or_four_stars['UserID'].unique():
     data_from_userID = merged_data_five_or_four_stars.loc[merged_data_five_or_four_stars['UserID'] == userID]
 
@@ -35,6 +40,7 @@ for userID in merged_data_five_or_four_stars['UserID'].unique():
     genres_count = pd.DataFrame({'Genres': genres_count.index, 'count': genres_count.values})
 
     genres_list_for_userID = genres_count.loc[genres_count['count'] == max_genres_count]['Genres'].to_list()
+    genres_list_for_userID = sorted(genres_list_for_userID)
     users_data['Genres'] = np.where(users_data['UserID'] == userID,
                                     str(genres_list_for_userID),
                                     users_data['Genres'])
@@ -44,3 +50,8 @@ for userID in merged_data_five_or_four_stars['UserID'].unique():
 # x = ast.literal_eval(users_data.loc[users_data['UserID'] == 12]['Genres'].to_list()[0])
 # for i in x:
 #     print(i)
+# print(users_data)
+print(users_data['Genres'].unique()[8])
+
+# ideias: pegar uma coisa como "Action|Adventure|Animation" e separar em Action, Adventure e Animation
+# e só depois contar os gêneros que mais aparecem
